a5721721b2aec0b9e0127a1ceb7202008c6d6fac
Apprentice <apprentice@devbootcamp.com>
2013-03-28 14:27:41 -0700

    removed duplicate migrations

--- db/migrate/20130328104646_create_dockets.rb
+++
@@ -1,8 +0,0 @@
-class CreateDockets < ActiveRecord::Migration
-  def change
-  	create_table :dockets do |t|
-  		t.integer :lawyer_id
-  		t.integer :case_id
-  	end
-  end
-end
--- db/migrate/20130328120325_create_dockets.rb
+++
@@ -1,9 +0,0 @@
-class CreateDockets < ActiveRecord::Migration
-  def change
-	  create_table :dockets do |t|
-		t.integer :lawyer_id
-		t.integer :case_id
-
-		t.timestamps
-  end
-end
